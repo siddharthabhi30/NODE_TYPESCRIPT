7ef5fd3953c94d2744b1c12899ea83efc8753349
siddharthabhi30 <siddharthabhi30@gmail.com>
2020-11-11 00:36:56 +0530

    jbk

--- nodeTYpescript/config-02-excluding/app.ts
+++ nodeTYpescript/config-02-excluding/app.ts
@@ -3,3 +3,30 @@ const mr=document.querySelector('button')!;
 mr .addEventListener('click',()=>{
     console.log("ddddddddd");
 })
+
+
+const mr2=document.querySelector('button');
+
+if(mr2){
+
+    mr .addEventListener('click',()=>{
+        console.log("ddddddddd");
+    })
+}
+//now both are Okay
+
+
+function ss(){
+    console.log("checkinf fubcti")
+}
+
+let cccc=ss;
+
+
+function a(n1:number){
+    if(n1>0){
+        return 11;
+    }
+
+    return 
+}

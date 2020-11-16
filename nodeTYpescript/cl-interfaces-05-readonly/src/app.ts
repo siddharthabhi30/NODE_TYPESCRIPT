c40730dd1c9ba93d3c8fdc34a867b9b7e8ba13af
siddharthabhi30 <siddharthabhi30@gmail.com>
2020-11-17 01:24:51 +0530

    new

---
+++ nodeTYpescript/cl-interfaces-05-readonly/src/app.ts
@@ -0,0 +1,51 @@
+class Department {
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    // validation etc
+    // this.id = 'd2';
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department('d1', 'Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna';
+
+accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();
+
+class my{
+  //readonly can be changed only once in a lifetime
+  constructor(public  readonly name:string){
+    this.happ="dd";
+
+  }
+}
+
+let ee=new my("sid");
+

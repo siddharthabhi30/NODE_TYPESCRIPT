00b6b3b27f8b417aad13387a7545d0481fdcbada
siddharthabhi30 <siddharthabhi30@gmail.com>
2020-11-16 23:56:56 +0530

    vbfd

---
+++ nodeTYpescript/cl-interfaces-03-private-and-public/src/app.ts
@@ -0,0 +1,40 @@
+class Department {
+  public name: string;
+
+  //this should be done
+   //.add also may not work if we dont do this
+  private employees: string[] = [];
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
+
+  addEmployee(employee: string) {
+    // validation etc
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department('Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna';
+
+accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();

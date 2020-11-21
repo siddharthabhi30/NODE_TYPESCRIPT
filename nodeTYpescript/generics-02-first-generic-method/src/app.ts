5192d9d3cd5775d74c037f7a7fe799361994768f
siddharthabhi30 <siddharthabhi30@gmail.com>
2020-11-21 22:27:10 +0530

    hjb

--- nodeTYpescript/generics-02-first-generic-method/src/app.ts
+++ nodeTYpescript/generics-02-first-generic-method/src/app.ts
@@ -16,13 +16,18 @@
 // })
 
 
- 
+ //below function can extend to integer type string type
 function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 //it is unioning the data
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { naame:"sid",age: 30 });
 console.log(mergedObj);
+
+//we get this bcoz we said to ts T and U cant be anything ...it has to be something
+//below example will work in  the lower example
+const mergedObj2= merge({ name: 'Max', hobbies: ['Sports'] }, 30);
+
 
 
 function merge2(objA:Object,objB:Object){
@@ -38,8 +43,19 @@ const mine=merge({name:'sid'},{shame:'ss'})
 
 
 //we are now saying to ts two different type of object is being passed
+//and function is merging both of them 
+//this T and U are filled at  
 function merge3<T,U>(objA:T,objB:U){
   return Object.assign(objA,objB);
 }
-const mine2=merge({name:'sid'},{shame:'ss'})
+//we are telling T should be like this and U should be like this but we dont need to do that ...ts infers
+const mine2=merge<{name: string},{shame:string}>({name:'sid'},{shame:'ss'})
+
+
+//this also works
+function merge4(objA:Object,objB:Object){
+  return Object.assign(objA,objB);
+}
+
+const mine3=merge({name:'sid'},{shame:'ss'})
 
